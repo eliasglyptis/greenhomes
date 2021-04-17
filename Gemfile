c40c44793807fa085abcbd59324cf630e4c46c9c
Elias Glyptis <eliasglyptis@gmail.com>
2021-04-17 14:50:21 -0400

    Autentication with Facebook completed. Updated user model if user exists or not can login with facebook or email, added omniauth_callbacks controller to execute the facebook function to run from_omniaut function in user model. Updated auth views

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem "bulma-rails", '~> 0.9.1'
 gem 'bulma-extensions-rails'
 
 gem 'devise', '=4.7.2'
+gem 'omniauth-facebook', '= 5.0.0'
+gem 'omniauth', '= 1.9.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,35 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable
 
   validates :full_name, presence: :true, length: { 
     maximum: 50,
     minimum: 2,
     message: " must be between 2 to 50 characters."
   }, on: :update, on: :create
+
+  def self.from_omniauth(auth)
+    user = User.where(email: auth.info.email).first
+
+    if user
+      return user
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.full_name = auth.info.name
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.image = auth.info.image
+        user.password = Devise.friendly_token[0, 20]
+        # issues below
+        # user.provider = auth.provider
+
+        # If you are using confirmable and the provider(s) you use validate emails,
+        # uncomment the line below to skip the confirmation emails.
+        user.skip_confirmation!
+      end
+    end
+  end
 end
